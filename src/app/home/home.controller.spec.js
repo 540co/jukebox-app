b0d2ec6b0965ad8ef97e3cc2208e1b2f06247e5f
Mark Ostrander <ostranme@gmail.com>
2017-01-14 23:24:55 -0500

    initial landing page design

---
+++ src/app/home/home.controller.spec.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  describe('Home Controller', function(){
+    var vm;
+
+    beforeEach(module('app'));
+    beforeEach(inject(function(_$controller_) {
+      vm = _$controller_('HomeController');
+    }));
+
+    it('should call the artists', function() {
+      //TODO
+    });
+  });
+})();
