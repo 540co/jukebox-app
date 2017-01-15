2a4ae2a427f4b1e4556d8ed4195ea7c0aa2d42c4
Jeremy Altman <jeremy@540.co>
2017-01-15 09:18:58 -0500

    modify config file before build/deployment

--- .deploy/build.sh
+++ .deploy/build.sh
@@ -6,4 +6,15 @@
 #  sed -i -e "s/<INSERT GA TRACKING ID HERE>/$GA_TRACKING_ID/g" dist/js/ga.js
 #fi
 
+# Update Config file based on environment
+BASE_URL=$BASE_URL_DEV
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  BASE_URL=$BASE_URL_PROD
+fi
+
+mv src/app/app.config-example.js src/app/app.config.js
+sed -i -e "s/<INSERT BASE URL HERE>/$BASE_URL/g" src/app/app.config.js
+
+# Build application
 gulp build
